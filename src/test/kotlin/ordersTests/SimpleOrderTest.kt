cba635980723d9d7338fac67f983102cffcccfb9
Genaro Tapia <genaro@sicar.mx>
2024-10-24 16:17:02 -0600

    init

---
+++ src/test/kotlin/ordersTests/SimpleOrderTest.kt
@@ -0,0 +1,12 @@
+package ordersTests
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class SimpleOrderTest {
+    @Test
+    @DisplayName("Simple Order Test")
+    fun simpleOrderTest() {
+        println("Simple Order Test")
+    }
+}
